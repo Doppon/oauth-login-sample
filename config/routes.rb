33fd5f69928b43aaf34f0cd4f7a8528cdd20d3d4
Doppon <zaxpkuraudo7777@gmail.com>
2019-07-05 06:33:12 +0900

    ルートを指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'oauth_test/index'
+  root :to => 'oauth_test#index'
+  
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
