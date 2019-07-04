0ef0146b5dd3165d91e38d8e10b9fa440e7e1ee4
Doppon <zaxpkuraudo7777@gmail.com>
2019-07-05 06:23:20 +0900

    コールバックするためのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'oauth_test/index'
   devise_for :users, controllers: {
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
 end
