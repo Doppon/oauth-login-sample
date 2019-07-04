41532908ce5ecb4886003f5fc518d8a0093381b9
Doppon <zaxpkuraudo7777@gmail.com>
2019-07-05 05:20:30 +0900

    マイグレーションファイルにname, uid, providerなど必要項目を追加

--- db/migrate/20190704201106_devise_create_users.rb
+++ db/migrate/20190704201106_devise_create_users.rb
@@ -4,8 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :provider, null: false, default: ""
+      t.string :uid, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,8 +39,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
+    add_index :users, :name,                 unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, [:uid, :provider], unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
